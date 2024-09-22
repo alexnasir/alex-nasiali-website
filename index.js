2941e810407de2289ab64f36486f1a473d833720
alex <alexnasiali45@gmail.com>
2024-09-22 22:19:40 +0300

    new

---
+++ index.js
@@ -0,0 +1,9 @@
+let clickheader = HTMLDListElement.document.getElementById("page-btn")
+ 
+let clickheader = HTMLDListElement.addEventListener("click",mouseClick())
+header.addEventListener("mouseover",click.style.backgroundColor="#fff")
+
+function mouseClick(){
+    alert("this is on mouse click")
+
+}
